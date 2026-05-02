90eedd40aca5ba4fd58943aa7aa8f87bc0075491
Meshkat Uddin Ahammed <meshkatuddinahammed@outlook.com>
2026-05-03 05:54:00 +0600

    fixing railway depliy 9

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     # uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
 
     # Production mode without auto-reload.
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
